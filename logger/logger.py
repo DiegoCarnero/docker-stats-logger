cb90af9329a834e66264ec67932af179be2bdb36
Diego Carnero <diegocarnero@users.noreply.github.com>
2023-08-31 14:09:51 +0200

    Implement config file handling

--- logger/logger.py
+++ logger/logger.py
@@ -2,20 +2,21 @@ import docker
 import concurrent.futures
 import csv
 import logger.utils as utils
-import json
-from os import system, makedirs
+from os import makedirs
 from time import sleep
 
 
 directory = ""
 project = ""
+interval = 0
+one_shot = False
 
 
 def log_summary(container, client):
     with open(f'{directory}/{container}_stats.csv', 'w') as stats_file:
         stats_file.write("datetime;container_name;CPU%;MEM usage;MEM %;NET IN;NET OUT;BLOCK IN;BLOCK OUT\n")
         while True:
-            stats = client.api.stats(container=container, decode=None, stream=False, one_shot=False)
+            stats = api_call(client, container)
             datetime = stats['read'].split(".")[0]
             block_in, block_out = utils.block_io(stats)
             net_in, net_out = utils.network_io(stats)
@@ -26,7 +27,7 @@ def log_summary(container, client):
 def log_raw(container, client):
     with open(f'{directory}/{container}_stats.log', 'w') as stats_file:
         while True:
-            stats = client.api.stats(container=container, decode=None, stream=False, one_shot=True)
+            stats = api_call(container=container)
             stats_file.write(f"{stats}\n")
             stats_file.flush()
 
@@ -37,7 +38,7 @@ def log_full_as_csv(container, client):
     with open(f'{directory}/{container}_stats.csv', 'w') as stats_file:
         header_written = False
         while True:
-            stats = client.api.stats(container=container, decode=None, stream=False, one_shot=True)
+            stats = api_call(client, container)
             flattened_stats = utils.flatten(stats)
             if not header_written:
                 csv_columns = flattened_stats.keys()
@@ -48,17 +49,33 @@ def log_full_as_csv(container, client):
             stats_file.flush()
 
 
+def api_call(client, container_name_or_id):
+    global interval
+    global one_shot
+    sleep_interval = max(0, interval - 1)  # -1 to account for the API's minimum delay. Calculating running average would be ideal
+    sleep(sleep_interval)
+    return client.api.stats(container=container_name_or_id, decode=None, stream=False, one_shot=one_shot)
+
+
 def run(config: dict):
     client = docker.from_env()
     containers_all = client.containers.list()
     container_names = list()
     global directory
+    global interval
+    global one_shot
+    global project
     log_modes = {"summary": log_summary, "full": log_full_as_csv, "raw": log_raw}
 
     if config.get("config_file") is None:
-        project = config.get("project_name")
+        project = config.get("project_name", "")
         directory = config.get("directory")
         mode = config.get("mode")
+        interval = config.get("interval")
+        one_shot = config.get("one-shot")
+
+    if mode == 'summary':
+        one_shot = False
 
     if project != "":
         directory = f'{directory}/{project}'
--- main.py
+++ main.py
@@ -1,15 +1,22 @@
 import argparse
 import os
 from logger.logger import run
+import yaml
 
 parser = argparse.ArgumentParser(description="Log Docker's system utilization.",
                                  formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-parser.add_argument("-c", "--config_file", help="Path to config file.")  # TODO actually implement this
+parser.add_argument("-c", "--config_file", help="Path to yaml config file.")
 parser.add_argument("-d", "--directory", help='Directory where log files will be stored.', default=os.getcwd() + "/stats")
 parser.add_argument("-m", "--mode", help='Archive mode. Options: summary, raw, full', default="full")
 parser.add_argument("-p", "--project_name", help="Project name. Only containers whose name begins with the provided project name will be monitored. Otherwise, all containers will be monitored.", default="")
+parser.add_argument("-i", "--interval", help="Interval between log entries in seconds. Due to the Docker API's response time it's impossible to get an interval time below 1 second.", default=0)
+parser.add_argument("-o", "--one-shot", help="Improve response time, decreasing the interval between entries. Ignored when using 'summary' logging mode. See Docker API's documentation for more details: https://docs.docker.com/engine/api/v1.43/#tag/Container/operation/ContainerStats", default=True)
 
 
 if __name__ == "__main__":
     config = vars(parser.parse_args())
+    config_file = config.get("config_file")
+    if config_file is not None:
+        with open(config_file, 'r') as f:
+            config = yaml.safe_load(f)
     run(config)
