8694bcf85f2318e5b8ea958ef784eb89580c2267
Diego Carnero <diegocarnero@users.noreply.github.com>
2023-09-04 13:27:46 +0200

    Add config from environment var

--- app/main.py
+++ app/main.py
@@ -7,10 +7,11 @@ parser = argparse.ArgumentParser(description="Log Docker's system utilization.",
                                  formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 parser.add_argument("-c", "--config_file", help="Path to yaml config file.")
 parser.add_argument("-d", "--directory", help='Directory where log files will be stored.', default=os.getcwd() + "/stats")
-parser.add_argument("-m", "--mode", help='Archive mode. Options: summary, raw, full', default="full")
-parser.add_argument("-p", "--project_name", help="Project name. Only containers whose name begins with the provided project name will be monitored. Otherwise, all containers will be monitored.", default="")
+parser.add_argument("-e", "--env", help='Get settings from environment variables.', action="store_true")
 parser.add_argument("-i", "--interval", help="Interval between log entries in seconds. Due to the Docker API's response time it's impossible to get an interval time below 1 second.", default=0)
+parser.add_argument("-m", "--mode", help='Archive mode. Options: summary, raw, full', default="full")
 parser.add_argument("-o", "--one-shot", help="Improve response time, decreasing the interval between entries. Ignored when using 'summary' logging mode. See Docker API's documentation for more details: https://docs.docker.com/engine/api/v1.43/#tag/Container/operation/ContainerStats", default=True)
+parser.add_argument("-p", "--project_name", help="Project name. Only containers whose name begins with the provided project name will be monitored. Otherwise, all containers will be monitored.", default="")
 
 
 if __name__ == "__main__":
@@ -19,4 +20,14 @@ if __name__ == "__main__":
     if config_file is not None:
         with open(config_file, 'r') as f:
             config = yaml.safe_load(f)
+    elif config.get("env"):
+        config["directory"] = os.getenv("LOGGING_DIRECTORY")
+        try:
+            config["interval"] = int(os.getenv("LOGGING_INTERVAL"))
+        except ValueError:
+            config["interval"] = 1
+        config["mode"] = os.getenv("LOGGING_MODE")
+        config["one-shot"] = True if os.getenv("LOGGING_ONE_SHOT").lower == 'true' else False
+        config["project_name"] = os.getenv("DOCKER_PROJECT")
+
     run(config)
