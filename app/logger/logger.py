0b549edf457f78edb9b5c06d0ea7da97c5602564
Diego Carnero <diegocarnero@users.noreply.github.com>
2023-09-05 16:28:56 +0200

    Add check for stopped or removed containers

--- app/logger/logger.py
+++ app/logger/logger.py
@@ -1,5 +1,5 @@
 import docker
-from threading import Thread
+from threading import Thread, Event
 import csv
 import logger.utils as utils
 from os import makedirs
@@ -12,33 +12,46 @@ interval = 0
 one_shot = False
 
 
+def sleep_log_interval():
+    global interval
+    global one_shot
+    # -1 second to account for the API's minimum delay, -1 more second if one_shot is enabled.
+    #  Calculating running average would be ideal
+    sleep_interval = max(0, interval - 1 - int(one_shot))
+    sleep(sleep_interval)
+
 def log_summary(container, client):
+    failure_event = Event()
     with open(f'{directory}/{container}_stats.csv', 'w') as stats_file:
         stats_file.write("datetime;container_name;CPU%;MEM usage;MEM %;NET IN;NET OUT;BLOCK IN;BLOCK OUT\n")
-        while True:
-            stats = api_call(client, container)
+        while not failure_event.is_set():
+            stats = api_call(client, container, failure_event)
             datetime = stats['read'].split(".")[0]
             block_in, block_out = utils.block_io(stats)
             net_in, net_out = utils.network_io(stats)
             stats_file.write(f"{datetime};{container};{utils.calculateCPUPercentUnix(stats)};{stats['memory_stats']['usage']};{utils.calculate_memory_perc(stats)};{net_in};{net_out};{block_in};{block_out}\n")
             stats_file.flush()
+            sleep_log_interval()
 
 
 def log_raw(container, client):
+    failure_event = Event()
     with open(f'{directory}/{container}_stats.log', 'w') as stats_file:
-        while True:
-            stats = api_call(container=container)
+        while not failure_event.is_set():
+            stats = api_call(client, container, failure_event)
             stats_file.write(f"{stats}\n")
             stats_file.flush()
+            sleep_log_interval()
 
 
 def log_full_as_csv(container, client):
+    failure_event = Event()
     # TODO simplify column names after flattening
     sep = ";"
     with open(f'{directory}/{container}_stats.csv', 'w') as stats_file:
         header_written = False
-        while True:
-            stats = api_call(client, container)
+        while not failure_event.is_set():
+            stats = api_call(client, container, failure_event)
             flattened_stats = utils.flatten(stats)
             if not header_written:
                 csv_columns = flattened_stats.keys()
@@ -47,16 +60,20 @@ def log_full_as_csv(container, client):
                 header_written = True
             writer.writerow(flattened_stats)
             stats_file.flush()
+            sleep_log_interval()
 
 
-def api_call(client, container_name_or_id):
-    global interval
-    global one_shot
-    # -1 second to account for the API's minimum delay, -1 more second if one_shot is enabled.
-    #  Calculating running average would be ideal
-    sleep_interval = max(0, interval - 1 - int(one_shot))
-    sleep(sleep_interval)
-    return client.api.stats(container=container_name_or_id, decode=None, stream=False, one_shot=one_shot)
+def api_call(client, container_name_or_id, failure_event: Event):
+    reponse = {}
+    try:
+        reponse = client.api.stats(container=container_name_or_id, decode=None, stream=False, one_shot=one_shot)
+        if reponse['read'] == '0001-01-01T00:00:00Z':
+            print(f"{container_name_or_id} returned most likely erroneous data. Exiting.", flush=True)
+            failure_event.set()
+    except Exception as e:
+        print(e)
+        failure_event.set()
+    return reponse
 
 
 def run(config: dict):
@@ -71,10 +88,10 @@ def run(config: dict):
 
     if config.get("config_file") is None:
         project = config.get("project_name", "")
-        directory = config.get("directory")
-        mode = config.get("mode")
-        interval = config.get("interval")
-        one_shot = config.get("one-shot")
+        directory = config.get("directory", "./stats/")
+        mode = config.get("mode", "full")
+        interval = config.get("interval", 0)
+        one_shot = config.get("one-shot", False)
 
     if mode == 'summary':
         one_shot = False
